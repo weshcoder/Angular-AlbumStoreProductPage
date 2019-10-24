641811be11f0b88b2d081604cffabdfae0eeedf0
Your Name <you@example.com>
2019-10-24 11:06:13 +0300

    test1-6

---
+++ src/app/product.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json'
+
+  constructor(private _http: Http) { }
+
+}
