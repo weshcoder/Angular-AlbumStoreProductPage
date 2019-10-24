884d149aad784ceffaae715e7b7ddec40ad6b53d
Your Name <you@example.com>
2019-10-24 11:11:53 +0300

    test1-6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import {} from 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
@@ -7,5 +8,10 @@ export class ProductService {
   private _albumUrl = '../assets/album.json'
 
   constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    this._http.get(this._albumUrl).map(
+    );
+  }
 
 }
