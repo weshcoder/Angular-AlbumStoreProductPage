c368ee90cef02b92bab84672b0b07eeaf38b3d04
Your Name <you@example.com>
2019-10-24 12:58:38 +0300

    Dome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: 'product', component: ProductListComponent},
+  { path: 'products', component: ProductListComponent},
   { path: 'product/:id', component: ProductPageComponent},
-  { path: '', redirectTo: 'product', pathMatch: 'full'}
+  { path: '', redirectTo: 'products', pathMatch: 'full'}
 ]
 
 @NgModule({
